9699bb9aa72f4b463c36d59dccba10748f92e8c7
StevenSimba <sigsimba@gmail.com>
2021-09-25 07:47:35 +0300

    Changed ipad color

---
+++ styles-b694b2d3131ebf508348.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{191:function(n,o,c){},213:function(n,o,c){},214:function(n,o,c){},215:function(n,o,c){},217:function(n,o,c){}}]);
+//# sourceMappingURL=styles-b694b2d3131ebf508348.js.map
